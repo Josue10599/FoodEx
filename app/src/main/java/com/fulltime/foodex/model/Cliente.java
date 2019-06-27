165e41be774051e2d103cbcab84af45281d7a8a5
Josue Lopes <josue10599@gmail.com>
2019-06-27 18:31:44 -0300

    Adicao e implementacao da classe cliente

---
+++ app/src/main/java/com/fulltime/foodex/model/Cliente.java
@@ -0,0 +1,57 @@
+package com.fulltime.foodex.model;
+
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Cliente {
+    private String nome;
+    private String telefone;
+    private String email;
+    private String cpf;
+    private BigDecimal valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setValor(String valor) {
+        this.valor = new BigDecimal(valor).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public String getValor() {
+        return valor.toString();
+    }
+
+    public String getPrimeiraLetra() {
+        return getNome().toUpperCase().substring(0,1);
+    }
+}
