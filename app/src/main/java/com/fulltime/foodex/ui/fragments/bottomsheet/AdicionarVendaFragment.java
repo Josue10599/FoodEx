c05458ec2a66e25d08d106e350da2e53d4770ecc
Josue Lopes <josue10599@gmail.com>
2019-07-05 18:26:39 -0300

    Criacao dos componentes para AdicionarVenda

---
+++ app/src/main/java/com/fulltime/foodex/ui/fragments/bottomsheet/AdicionarVendaFragment.java
@@ -0,0 +1,20 @@
+package com.fulltime.foodex.ui.fragments.bottomsheet;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class AdicionarVendaFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
