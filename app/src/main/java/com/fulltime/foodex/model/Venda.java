67dcb7973c49b1aae620ee1e56289703d61a87c9
Josue Lopes <josue10599@gmail.com>
2019-07-12 18:28:39 -0300

    Simplificacao da classe venda, retirada da quantidade a pagar

--- app/src/main/java/com/fulltime/foodex/model/Venda.java
+++ app/src/main/java/com/fulltime/foodex/model/Venda.java
@@ -5,8 +5,10 @@ import com.fulltime.foodex.formatter.FormataDinheiro;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -15,31 +17,36 @@ public class Venda implements Serializable {
     private FormataDinheiro formataDinheiro = new FormataDinheiro();
     private Date dataVenda;
     private Cliente cliente;
-    private Produto[] produtosVendidos;
-    private BigDecimal valorEmDeficit;
+    private List<Produto> produtosVendidos;
     private BigDecimal valorDaCompra;
-    private boolean estadoVenda;
 
     public Venda() {
         this.id = UUID.randomUUID().toString();
         this.dataVenda = Calendar.getInstance().getTime();
-        this.valorEmDeficit = new BigDecimal("0");
         this.valorDaCompra = new BigDecimal("0");
-        this.estadoVenda = false;
     }
 
     public Venda(Cliente cliente, boolean pago, Produto... produtoVendido) {
         this.id = UUID.randomUUID().toString();
         this.dataVenda = Calendar.getInstance().getTime();
         this.cliente = cliente;
-        this.produtosVendidos = produtoVendido;
-        this.estadoVenda = pago;
+        this.produtosVendidos.addAll(Arrays.asList(produtoVendido));
         this.valorDaCompra = new BigDecimal("0");
         setValorDaCompra();
-        this.valorEmDeficit = new BigDecimal("0");
         if (!pago) {
-            valorEmDeficit = valorDaCompra;
-            cliente.setValorEmDeficit(formataDinheiro.formataValor(valorEmDeficit));
+            cliente.setValorEmDeficit(formataDinheiro.formataValor(valorDaCompra));
+        }
+    }
+
+    public Venda(Cliente cliente, boolean pago, List<Produto> produtoVendido) {
+        this.id = UUID.randomUUID().toString();
+        this.dataVenda = Calendar.getInstance().getTime();
+        this.cliente = cliente;
+        this.produtosVendidos = produtoVendido;
+        this.valorDaCompra = new BigDecimal("0");
+        setValorDaCompra();
+        if (!pago) {
+            cliente.setValorEmDeficit(formataDinheiro.formataValor(valorDaCompra));
         }
     }
 
@@ -47,20 +54,6 @@ public class Venda implements Serializable {
         for (Produto produto : produtosVendidos) {
             valorDaCompra = valorDaCompra.add(formataDinheiro.getBigDecimal(produto.getValor()));
         }
-    }
-
-    public boolean vendaPagaCompletamente() {
-        return estadoVenda;
-    }
-
-    private void setEstadoVenda(boolean estadoVenda) {
-        this.estadoVenda = estadoVenda;
-    }
-
-    public void registrarPagamento(String valor) {
-        cliente.valorPago(valor);
-        valorEmDeficit = valorEmDeficit.subtract(formataDinheiro.getBigDecimal(valor));
-        vendaPaga();
     }
 
     public String getDataVenda() {
@@ -72,23 +65,15 @@ public class Venda implements Serializable {
     }
 
     public Produto getProdutosVendidos(int posicao) {
-        return produtosVendidos[posicao];
+        return produtosVendidos.get(posicao);
     }
 
     public int getNumeroProdutosVendidos() {
-        return produtosVendidos.length;
+        return produtosVendidos.size();
     }
 
     public String getValorDaCompra() {
         return formataDinheiro.formataValor(valorDaCompra);
-    }
-
-    public String getValorEmDeficit() {
-        return formataDinheiro.formataValor(valorEmDeficit);
-    }
-
-    private void vendaPaga() {
-        estadoVenda = valorEmDeficit.compareTo(new BigDecimal("0")) == 0;
     }
 
     public String getId() {
