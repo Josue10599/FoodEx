2695625a2abf9008078e727da23847b1d510b175
Josue Lopes <josue10599@gmail.com>
2019-07-22 19:01:31 -0300

    Adicionado valor da venda enquanto a esta realizando

--- app/src/main/java/com/fulltime/foodex/ui/fragments/PerfilUsuarioFragment.java
+++ app/src/main/java/com/fulltime/foodex/ui/fragments/PerfilUsuarioFragment.java
@@ -1,23 +1,31 @@
 package com.fulltime.foodex.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
+import com.amulyakhare.textdrawable.TextDrawable;
 import com.fulltime.foodex.R;
 import com.fulltime.foodex.firebase.authentication.Usuario;
+import com.fulltime.foodex.ui.activity.SignInActivity;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.display.CircleBitmapDisplayer;
+
+import java.util.Objects;
 
 public class PerfilUsuarioFragment extends Fragment {
 
@@ -32,22 +40,53 @@ public class PerfilUsuarioFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View perfilUsuarioFragment = inflater.inflate(R.layout.fragment_perfil, container, false);
         configuraFotoPerfil(perfilUsuarioFragment);
+        configuraNomeUsuario(perfilUsuarioFragment);
+        configuraBotaoLogout(perfilUsuarioFragment);
+        return perfilUsuarioFragment;
+    }
+
+    private void configuraBotaoLogout(View perfilUsuarioFragment) {
+        ImageButton logout = perfilUsuarioFragment.findViewById(R.id.fragment_perfil_config_button);
+        logout.setOnClickListener(view -> logoutApp());
+    }
+
+    private void logoutApp() {
+
+        FirebaseAuth.getInstance().signOut();
+        Intent telaLogin = new Intent(getContext(), SignInActivity.class);
+        startActivity(telaLogin);
+        Objects.requireNonNull(getActivity()).finish();
+    }
+
+    private void configuraNomeUsuario(View perfilUsuarioFragment) {
         MaterialTextView textViewNomeUsuario = perfilUsuarioFragment
                 .findViewById(R.id.fragment_perfil_nome_usuario);
         textViewNomeUsuario.setText(usuario.getDisplayName());
-        return perfilUsuarioFragment;
     }
 
     private void configuraFotoPerfil(View perfilUsuarioFragment) {
         ImageView fotoPerfil = perfilUsuarioFragment.findViewById(R.id.fragment_perfil_foto_usuario);
-        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(getContext())
+        TextDrawable drawablePadrao = TextDrawable.builder()
+                .buildRound(String.valueOf(usuario.getDisplayName().charAt(0)),
+                        Objects.requireNonNull(getContext())
+                                .getResources().getColor(R.color.color_secondary));
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(
+                Objects.requireNonNull(getContext()))
                 .writeDebugLogs()
                 .build();
         ImageLoader.getInstance().init(configuration);
+        CircularProgressDrawable drawableLoading = new CircularProgressDrawable(getContext());
+        drawableLoading.setStyle(CircularProgressDrawable.DEFAULT);
+        drawableLoading.start();
         DisplayImageOptions imageOptions = new DisplayImageOptions.Builder()
                 .imageScaleType(ImageScaleType.EXACTLY)
+                .cacheOnDisk(true)
                 .displayer(new CircleBitmapDisplayer())
+                .showImageForEmptyUri(drawablePadrao)
+                .showImageOnFail(drawablePadrao)
+                .showImageOnLoading(drawableLoading)
                 .build();
-        ImageLoader.getInstance().displayImage(usuario.getPhotoUrl(), fotoPerfil, imageOptions);
+        if (!usuario.getPhotoUrl().isEmpty())
+            ImageLoader.getInstance().displayImage(usuario.getPhotoUrl(), fotoPerfil, imageOptions);
     }
 }
