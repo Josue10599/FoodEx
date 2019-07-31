a001636722cf04d46aca406966aa45aa73bc011e
Josue Lopes <josue10599@gmail.com>
2019-07-31 16:41:38 -0300

    Add License GPLv3

--- app/src/main/java/com/fulltime/foodex/helper/update/RemoveCliente.java
+++ app/src/main/java/com/fulltime/foodex/helper/update/RemoveCliente.java
@@ -1,5 +1,23 @@
 package com.fulltime.foodex.helper.update;
 
+/*
+  FoodEx is a sales management application.
+  Copyright (C) 2019 Josue Lopes.
+  <p>
+  FoodEx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  <p>
+  FoodEx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  <p>
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import com.fulltime.foodex.model.Cliente;
 import com.fulltime.foodex.ui.recyclerview.adapter.ClienteAdapter;
 
--- app/src/main/java/com/fulltime/foodex/ui/fragments/PerfilUsuarioFragment.java
+++ app/src/main/java/com/fulltime/foodex/ui/fragments/PerfilUsuarioFragment.java
@@ -1,5 +1,23 @@
 package com.fulltime.foodex.ui.fragments;
 
+/*
+  FoodEx is a sales management application.
+  Copyright (C) 2019 Josue Lopes.
+  <p>
+  FoodEx is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  <p>
+  FoodEx is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  <p>
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -95,6 +113,7 @@ public class PerfilUsuarioFragment extends Fragment {
 
     private void openDialogConfig() {
         EditaEmpresaFragment editaEmpresaFragment = new EditaEmpresaFragment(empresa);
+        assert getFragmentManager() != null;
         editaEmpresaFragment.show(getFragmentManager(), "DIALOG");
     }
 
